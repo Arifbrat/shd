190e782f699331ddd55955e79b488604ec07996d
Arifbrat <89793610+Arifbrat@users.noreply.github.com>
2022-12-04 10:39:46 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "")
+SESSION_NAME = getenv("SESSION_NAME", "AgAU9HdolQkeydWIFv024d0-NvRdW-217k4RMpigeone2FjFvofttW6IQLQ-4K9KzsTviCUwRbryRKTPDu_ARb0cmir-Aqqk9BsBK_Ntu21yOK83Ihcb3NkV1gUVD5cqPaXNE0_ZtmXJLUwCXZoxGJJ7esJVXMFeOcvXm4EoBz2O8RPVwVPt4w1mfRuYm66qQt90nu9qHcPsBXYSwlKnZehZqk-TnwgePx6WZCbVxMRiEsi5r2-Nir6wlPqohVw4uAINE5d-AVChbeHVbWSjv-yCh9dlgJX-fwfczlURagbFB8IDaBBSTJY6ylFICUzrohnas8ub1ytjjDp5xnJhwSurAAAAAUm2KMYA")
 BOT_TOKEN = getenv("BOT_TOKEN", "5548856893:AAGaplJtK3C8y3A0x3l9ST9fQkiNr3rJNaU")
 BOT_NAME = getenv("BOT_NAME", "SeCReT MuSiC") 
 API_ID = int(getenv("API_ID", "18482353"))
