3d513e7e04328c9bba6f68db42849e2417aad782
Arifbrat <89793610+Arifbrat@users.noreply.github.com>
2022-12-04 10:29:04 +0400

    Add files via upload

---
+++ handlers/cleaner.py
@@ -0,0 +1,52 @@
+# 𝐃𝐎𝐍𝐓 𝐌𝐄𝐒𝐒 𝐖𝐈𝐓𝐇 𝐂𝐎𝐃𝐄𝐒 𝐂𝐎𝐏𝐘𝐑𝐈𝐆𝐇𝐓 @SHAILENDRA34 | 
+# 𝐃𝐞𝐚𝐫 𝐏𝐞𝐫𝐨 𝐩𝐩𝐥𝐬 𝐏𝐥𝐢𝐬𝐡 𝐃𝐨𝐧'𝐭 𝐫𝐞𝐦𝐨𝐯𝐞 𝐭𝐡𝐢𝐬 𝐥𝐢𝐧𝐞 𝐟𝐫𝐨𝐦 𝐡𝐞𝐫𝐞 🌚
+
+
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from helpers.filters import command, other_filters
+from helpers.decorators import sudo_users_only, errors
+from helpers.command import commandpro
+
+downloads = os.path.realpath("downloads")
+raw_files = os.path.realpath("raw_files")
+
+@Client.on_message(command("rmf") & ~filters.edited)
+@errors
+@authorized_users_only
+async def clear_downloads(_, message: Message):
+    ls_dir = os.listdir(downloads)
+    if ls_dir:
+        for file in os.listdir(downloads):
+            os.remove(os.path.join(downloads, file))
+        await message.reply_text("`🗑 Bütün endirilmiş fayllar silindi `")
+    else:
+        await message.reply_text("`Faylın təmizləndiyi tapıldı`")
+
+        
+@Client.on_message(command("rmw") & ~filters.edited)
+@errors
+@authorized_users_only
+async def clear_raw(_, message: Message):
+    ls_dir = os.listdir(raw_files)
+    if ls_dir:
+        for file in os.listdir(raw_files):
+            os.remove(os.path.join(raw_files, file))
+        await message.reply_text("`Təmizlik qurulur 🗑️`")
+    else:
+        await message.reply_text("`Fayllar hazırdır`")
+
+
+@Client.on_message(commandpro(["/clean"]) & ~filters.edited)
+@errors
+@authorized_users_only
+async def cleanup(_, message: Message):
+    pth = os.path.realpath(".")
+    ls_dir = os.listdir(pth)
+    if ls_dir:
+        for dta in os.listdir(pth):
+            os.system("rm -rf *.webm *.jpg")
+        await message.reply_text("`✅ Təmizləndi`")
+    else:
+        await message.reply_text("`✅ Təmizləmə uğurludur`")
